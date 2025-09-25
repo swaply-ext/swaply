1890d0c03eb1fb7cadfaac19608cc473c08f67a4
eloidazaa <eloidaza4@gmail.com>
2025-09-25 10:48:04 +0200

    Modificación Inputs y botones.

--- frontend/src/app/components/action-buttons/action-buttons.component.ts
+++ frontend/src/app/components/action-buttons/action-buttons.component.ts
@@ -1,17 +1,25 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-action-buttons',
   standalone: true,
   templateUrl: './action-buttons.component.html',
-  styleUrls: ['./action-buttons.component.css'] 
+  styleUrls: ['./action-buttons.component.css']
 })
 export class ActionButtonsComponent {
 
-
   @Output() submit = new EventEmitter<void>();
 
+  constructor(private router: Router) {}
+
+  // Botón "Registrar"
   onRegistrarseClick() {
     this.submit.emit(); 
+  }
+
+  // Botón "Iniciar sesión"
+  onLoginClick() {
+    this.router.navigate(['/login']);
   }
 }
--- frontend/src/app/pages/new-password/new-password.component.ts
+++ frontend/src/app/pages/new-password/new-password.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PasswordInputComponent } from '../../components/password-input/password-input.component';
 import { ConfirmPasswordInputComponent } from '../../components/confirm-password-input/confirm-password-input.component';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-new-password',
@@ -16,6 +17,8 @@ export class NewPasswordComponent {
   newPassword: string = '';
   confirmPassword: string = '';
 
+  constructor(private location: Location) {}
+
   isFormValid(): boolean {
     return this.newPassword.length >= 6 && this.newPassword === this.confirmPassword;
   }
@@ -25,5 +28,9 @@ export class NewPasswordComponent {
 
     console.log('Contraseña nueva enviada:', this.newPassword);
     // Aquí podrías llamar a tu servicio API para actualizar la contraseña
+  }
+
+  goBack(): void {
+    this.location.back(); 
   }
 }
