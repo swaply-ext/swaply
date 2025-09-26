368f0041d098581c5df6fc61cc8119f55c580b76
IBL\victor.camon.ext <victor.camon.ext@inetum.com>
2025-09-26 09:09:16 +0200

    Azure-Key Vault yml getter

--- backend/src/main/java/com/swaply/backend/application/usecase/SecretClientConfiguration.java
+++ backend/src/main/java/com/swaply/backend/application/usecase/SecretClientConfiguration.java
@@ -1,18 +1,23 @@
 package com.swaply.backend.application.usecase;
+
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class SecretClientConfiguration {
+
+    @Value("${keyVault.endpoint}")
+    private String vaultEndpoint;    
 
     @Bean
     public SecretClient createSecretClient() {
         return new SecretClientBuilder()
-            .vaultUrl("https://swaply-key.vault.azure.net/")
+            .vaultUrl(vaultEndpoint)
             .credential(new DefaultAzureCredentialBuilder().build())
             .buildClient();
     }
-
 }
--- backend/src/main/java/com/swaply/backend/infrastructure/config/CosmosConfig.java
+++ backend/src/main/java/com/swaply/backend/infrastructure/config/CosmosConfig.java
@@ -3,11 +3,18 @@ package com.swaply.backend.infrastructure.config;
 import com.azure.cosmos.CosmosClient;
 import com.azure.cosmos.CosmosClientBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CosmosConfig {
+
+    @Value("${cosmos.key-secret}")
+    private String keySecret;
+
+    @Value("${cosmos.endpoint}")
+    private String cosmosEndpoint;
 
     private final SecretClient secretClient;
 
@@ -18,8 +25,7 @@ public class CosmosConfig {
     @Bean
     public CosmosClient cosmosClient() {
         // Obtener la key de Cosmos DB desde Key Vault
-        String cosmosKey = secretClient.getSecret("cosmos-readwrite-key").getValue();
-        String cosmosEndpoint = "https://swaply-db.documents.azure.com:443/";
+        String cosmosKey = secretClient.getSecret(keySecret).getValue();
 
         // Crear cliente de Cosmos DB
         return new CosmosClientBuilder()
