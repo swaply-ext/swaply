a2adf5c94804405fc4c041b150947a94a24ca1de
aleixfal7 <aleixmargalefranquet@gmail.com>
2025-10-03 11:50:11 +0200

    nuevo component ddmenú de navegacion, visual sin logica ---- aleix

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -11,6 +11,7 @@ import { PersonalInformationComponent } from './pages/personal-information/perso
 import { ConfirmationComponent } from './pages/confirmation/confirmation.component'; 
 import { ConfirmPasswordComponent } from './pages/confirm-password/confirm-password.component';
 import { ErrorAuthComponent } from './pages/error-auth/error-auth.component'; 
+import { AppNavbarComponent } from './components/app-navbar/app-navbar.component';
 
 // Creamos una ruta para la verificación de correo
 // Ahora usamos el componente real EmailVerificationComponent
@@ -27,4 +28,6 @@ export const appRoutes: Routes = [
   { path: 'confirmation', component: ConfirmationComponent }, // <-- ruta para pantalla de confirmación antes de Home
   { path: 'confirm-password', component: ConfirmPasswordComponent }, // <-- ruta para pantalla de confirmación de cambio de contraseña
   { path: 'error-auth', component: ErrorAuthComponent }, // <-- ruta para pantalla de error de autenticación
+  { path: 'app-navbar', component: AppNavbarComponent }, // <-- ruta para el componente de menú de navegación
+  { path: '**', redirectTo: '' } // redirige cualquier ruta no definida a la ruta principal 
 ];
