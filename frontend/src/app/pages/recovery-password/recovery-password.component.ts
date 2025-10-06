d76d0c279bcb375e5c3f14ae3bad297bfa792cae
aleixfal7 <aleixmargalefranquet@gmail.com>
2025-10-06 16:31:35 +0200

    integracion de api en páginas: datos personales, recuperar contraseña,  nueva contraseña, codigo verificacion.

--- frontend/src/app/pages/recovery-password/recovery-password.component.ts
+++ frontend/src/app/pages/recovery-password/recovery-password.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 interface User {
   email: string;
@@ -11,18 +13,18 @@ interface User {
 @Component({
   selector: 'app-recovery-password',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './recovery-password.component.html',
   styleUrls: ['./recovery-password.component.css']
 })
 export class RecoveryPasswordComponent {
   email: string = '';
 
-  registeredUsers: User[] = [
-    { email: 'test@example.com' }
+  registeredUsers: User[] = [ //aqui hace falta recibir de una consulta a bd los email de users regstradirs
+    { email: 'test@example.com' } //hay uno de ejemplo
   ];
 
-  constructor(private router: Router, private location: Location) {}
+  constructor(private router: Router, private location: Location, private http: HttpClient) {}
 
   enviarCodigo() {
     if (!this.email) {
@@ -36,8 +38,13 @@ export class RecoveryPasswordComponent {
       alert(' El correo no está registrado.');
       return;
     }
+    this.http.post('http://localhost:8081/api/recovery-pass', { email: this.email })
+    .subscribe({
+      next: response => console.log('Respuesta del backend:', response),
+      error: err => console.error('Error enviando dato:', err)
+    });
 
-    console.log('Código de recuperación enviado a', foundUser.email);
+    console.log('Código de recuperación enviado a', foundUser.email); //aqui iria la logica de envio de email con codigo
     this.router.navigate(['/verify']);
   }
 
