945256a9d43d821037ae5d2da700ffbdeb600f01
aleixfal7 <aleixmargalefranquet@gmail.com>
2025-10-08 16:21:06 +0200

    primer commit de la vista profile

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppNavbarComponent } from "../../components/app-navbar/app-navbar.component";
+import { ProfileInfoComponent } from "../../components/profile-info/profile-info.component";
+import { SkillsPanelComponent } from '../../components/skills-panel/skills-panel.component';
+import { InterestsPanelComponent } from '../../components/interests-panel/interests-panel.component';
+
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [CommonModule, AppNavbarComponent, ProfileInfoComponent, SkillsPanelComponent, InterestsPanelComponent],
+  standalone: true,
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
