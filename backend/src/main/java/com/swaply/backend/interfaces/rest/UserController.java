46640d96e254181d8106f6daff8fd3c285114d7a
v-camon <vicama018@gmail.com>
2025-10-18 21:04:03 +0200

    Users API Rebuild

--- backend/src/main/java/com/swaply/backend/interfaces/rest/UserController.java
+++ backend/src/main/java/com/swaply/backend/interfaces/rest/UserController.java
@@ -19,36 +19,36 @@ public class UserController {
         this.service = service;
     }
 
-    @PostMapping("/new")
+    @PostMapping
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO user) {
         UserDTO createdUser = service.createUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 
-    @GetMapping("/getAll")
+    @GetMapping
     public ResponseEntity<List<UserDTO>> getAll() {
         return ResponseEntity.ok(service.getAllUsers());
     }
 
-    @GetMapping("/getUserById/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUserById(@PathVariable String id) {
         return ResponseEntity.ok(service.getUserByID(id));
     }
 
     // Porque se retorna noContent?
-    @DeleteMapping("/deleteUserById/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUserById(@PathVariable String id) {
         service.deleteUserById(id);
         return ResponseEntity.noContent().build();
     }
 
-    @PatchMapping("/update/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<UserDTO> updatedUser(@PathVariable String id, @RequestBody UserDTO user) {
         UserDTO updatedUser = service.updateUser(id, user);
         return ResponseEntity.ok(updatedUser);
     }
 
-    @GetMapping("/getByEmail/{email}")
+    @GetMapping("/email/{email}")
     public ResponseEntity<UserDTO> getByEmail(@PathVariable String email) {
         return ResponseEntity.ok(service.getUserByEmail(email));
     }
