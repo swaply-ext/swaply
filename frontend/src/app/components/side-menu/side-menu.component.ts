b3d94f0a4b1d1755a7e3cb5843b01d887cfe33aa
Arnau <arnau.figueres.ext@inetum.com>
2025-10-23 10:59:25 +0200

    Add SideMenu component with routing and styles

---
+++ frontend/src/app/components/side-menu/side-menu.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-side-menu',
+  imports: [RouterModule],
+  standalone: true,
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.css'
+})
+export class SideMenuComponent {
+
+}
