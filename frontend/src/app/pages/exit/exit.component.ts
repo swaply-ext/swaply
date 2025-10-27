f3369bd23d445c9b076dd10d2061bb0474e5f277
Pablo-MedinaG <pmedina3a@gmail.com>
2025-10-27 10:21:32 +0100

    Creado componente exit

---
+++ frontend/src/app/pages/exit/exit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exit',
+  imports: [],
+  templateUrl: './exit.component.html',
+  styleUrl: './exit.component.css'
+})
+export class ExitComponent {
+
+}
