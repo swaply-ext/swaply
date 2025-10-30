6a42aa23afd21e74498319cb44bb34cb9ff96c4c
Pablo-MedinaG <pmedina3a@gmail.com>
2025-10-30 12:13:34 +0100

    Personal-information con errores personalizados y todas las alerts eliminadas. Se han hecho cambios necesarios en los inputs

--- frontend/src/app/components/username-input/username-input.component.ts
+++ frontend/src/app/components/username-input/username-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -11,5 +11,6 @@ import { FormsModule } from '@angular/forms';
 })
 export class UsernameInputComponent {
   username = '';
+  @Input() hasError: boolean = false;
   @Output() usernameChange = new EventEmitter<string>();
 }
