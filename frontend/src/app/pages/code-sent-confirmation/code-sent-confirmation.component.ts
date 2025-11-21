48b00e3ee8744cfffe2c66b1730f04ae8041ab52
Lara78624 <laraibsohail624@gmail.com>
2025-11-21 11:15:51 +0100

    Deleted unused methods.

--- frontend/src/app/pages/code-sent-confirmation/code-sent-confirmation.component.ts
+++ frontend/src/app/pages/code-sent-confirmation/code-sent-confirmation.component.ts
@@ -1,23 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-code-sent-confirmation',
   templateUrl: './code-sent-confirmation.component.html',
-  styleUrl: './code-sent-confirmation.component.css'
+  styleUrls: ['./code-sent-confirmation.component.css']
 })
-export class CodeSentConfirmationComponent implements OnInit {
+export class CodeSentConfirmationComponent {
 
 
   constructor(private router: Router) {}
-
-   ngOnInit(): void {
-    // Redirige automáticamente al Home después de 3 segundos
-    // setTimeout(() => {
-    //   this.router.navigate(['/']);
-    // }, 3000);
-  }
 
    continueToVerification() {
     this.router.navigate(['/verify']);
--- frontend/src/app/pages/link-sent-confirmation/link-sent-confirmation.component.ts
+++ frontend/src/app/pages/link-sent-confirmation/link-sent-confirmation.component.ts
@@ -1,17 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-link-sent-confirmation',
   templateUrl: './link-sent-confirmation.component.html',
   styleUrls: ['./link-sent-confirmation.component.css']
 })
-export class LinkSentConfirmationComponent implements OnInit {
+export class LinkSentConfirmationComponent {
 
-   ngOnInit(): void {
-    // Redirige automáticamente al Home después de 3 segundos
-    // setTimeout(() => {
-    //   this.router.navigate(['/']);
-    // }, 3000);
-  }
 
 }
