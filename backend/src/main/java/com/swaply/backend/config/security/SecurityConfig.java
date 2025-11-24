2ff029c2364622df343e21d0828db95ac205511c
Victor Camon Martinez <46001842+v-camon@users.noreply.github.com>
2025-11-24 11:30:42 +0100

    Require authentication for all requests

--- backend/src/main/java/com/swaply/backend/config/security/SecurityConfig.java
+++ backend/src/main/java/com/swaply/backend/config/security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/example/premium/**").hasRole("PREMIUM")
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 )
 
                 .sessionManagement(sess -> sess
