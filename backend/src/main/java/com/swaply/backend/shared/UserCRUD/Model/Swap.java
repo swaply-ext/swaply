3555b1c655b5518af08644022b6f8c7d978dab1d
Pablo-MedinaG <pmedina3a@gmail.com>
2025-12-05 10:51:29 +0100

    Creado modelo de Swap

---
+++ backend/src/main/java/com/swaply/backend/shared/UserCRUD/Model/Swap.java
@@ -0,0 +1,22 @@
+package com.swaply.backend.shared.UserCRUD.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Swap {
+    private String requestingUser;
+    private String requestedUser;
+    private String skill;
+    private String interest;
+    private Status status;
+
+    public enum Status {
+        ACCEPTED,
+        STANDBY,
+        DENIED
+    }
+}
