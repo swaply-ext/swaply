aa3e1fb095bab433f2fd7c72dd750a764f4c6cd0
Pablo-MedinaG <pmedina3a@gmail.com>
2025-12-09 08:27:23 +0100

    Modificado modelo Swap

--- backend/src/main/java/com/swaply/backend/shared/UserCRUD/Model/Swap.java
+++ backend/src/main/java/com/swaply/backend/shared/UserCRUD/Model/Swap.java
@@ -9,11 +9,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Swap {
     private String id;
-    private String requestingUserId;
-    private String requestedUserId;
+    private String userId;
     private String skill;
     private String interest;
     private Status status;
+    private Boolean isRequested;
 
     public enum Status {
         ACCEPTED,
