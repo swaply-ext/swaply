40230b09c447c99137eb8144056d0a62fcc43fb3
aleixfal7 <aleixmargalefranquet@gmail.com>
2025-12-17 12:31:28 +0100

    TAREA DE ENVIO DE EMAIL AL RECIBIR UNA SOLICITUD DE INTERCAMBIO

--- backend/src/main/java/com/swaply/backend/shared/mail/MailService.java
+++ backend/src/main/java/com/swaply/backend/shared/mail/MailService.java
@@ -67,4 +67,22 @@ public class MailService {
         inlineImages.put("swaply_hands", new ClassPathResource("templates/email/images/Swaply-hands.png"));
         sendHtmlEmail(email, "Restablece tu contraseña de Swaply", htmlBody, inlineImages);
     }
+
+    public void sendSwapRequestEmail(String toEmail, String receiverName, String requesterName, String skillOffered, String skillRequested) {
+        Context context = new Context();
+        context.setVariable("receiverName", receiverName);
+        context.setVariable("requesterName", requesterName);
+        context.setVariable("skillOffered", skillOffered);
+        context.setVariable("skillRequested", skillRequested);
+
+        //Email al hacer la solicitud de swap
+
+        String htmlBody = templateEngine.process("email/SwapRequestNotification", context);
+
+        Map<String, Resource> inlineImages = new HashMap<>();
+        inlineImages.put("logo_swp_invert", new ClassPathResource("templates/email/images/logo_swp_invert.png"));
+        inlineImages.put("swaply_hands", new ClassPathResource("templates/email/images/Swaply-hands.png"));
+
+        sendHtmlEmail(toEmail, "¡Tienes una nueva solicitud de intercambio en Swaply!", htmlBody, inlineImages);
+    }
 }
