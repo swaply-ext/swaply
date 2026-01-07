520d565805d427e72b07e6b5878108cc9fce6b7a
Arnau-Gonzalez-Nieto <arnau.gonzalez.ext@inetum.com>
2026-01-07 14:02:11 +0100

    fix interfaces de search-service a models

---
+++ frontend/src/app/models/swapDTO.model.ts
@@ -0,0 +1,5 @@
+export interface SwapDTO {
+  requestedUsername: string;
+  skill: string;
+  interest: string;
+}
---
+++ frontend/src/app/models/userSwapDTO.model.ts
@@ -0,0 +1,22 @@
+import { UserLocation } from "./user-location.model";
+import { userSkillDTO } from "./userSkillDTO.model";
+
+export interface UserSwapDTO {
+  userId: string;
+  name: string;
+  username: string;
+  profilePhotoUrl: string;
+  location: UserLocation;
+
+  skillName: string;
+  skillIcon: string;
+  skillLevel: number;
+  skillCategory: string;
+
+  isSwapMatch: boolean;
+  rating: number;
+  distance: string;
+
+  // lista opcional de skills que viene del back
+  userSkills?: userSkillDTO[];
+}
