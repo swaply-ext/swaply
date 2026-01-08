4f7fcd701ad59df9639333ab7ec5a991c799ef6a
eloidazaa <eloidaza4@gmail.com>
2026-01-08 16:27:20 +0100

    funciona

--- frontend/src/app/models/swap-profile-data.ts
+++ frontend/src/app/models/swap-profile-data.ts
@@ -1,8 +1,11 @@
 import { UserLocation } from "./user-location.model";
+import { UserSkills } from "./user-skills.model";
 export interface SwapProfileData {
   title: string;
   imgToTeach: string;
   profilePhotoUrl: string;
   location: UserLocation;
   username: string;
+  rating: number;
+  skills: UserSkills[];
 }
