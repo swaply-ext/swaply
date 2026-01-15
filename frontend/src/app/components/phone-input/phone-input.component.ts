1a43a9da9183ed79d3b5fe2c43919ffb927aa38c
Arnau <arnaufigueres2005@gmail.com>
2026-01-15 11:14:52 +0100

    De 0 a null

--- frontend/src/app/components/phone-input/phone-input.component.ts
+++ frontend/src/app/components/phone-input/phone-input.component.ts
@@ -10,7 +10,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './phone-input.component.css'
 })
 export class PhoneInputComponent {
-  phone: number = 0;
+  phone: number = null!;
   @Input() hasError: boolean = false; 
   @Output() phoneChange = new EventEmitter<number>();
 }
