19466578d8690b1cc6de493260009dbc6481bb8e
aleixfal7 <aleixmargalefranquet@gmail.com>
2026-01-16 12:24:03 +0100

    arnauuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu

--- backend/src/main/java/com/swaply/backend/application/account/AccountController.java
+++ backend/src/main/java/com/swaply/backend/application/account/AccountController.java
@@ -1,6 +1,4 @@
 package com.swaply.backend.application.account;
-import java.util.Collections;
-import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
