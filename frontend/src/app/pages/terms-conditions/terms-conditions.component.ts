63d1c05434bd43a4a940e5493284ba040512ce78
eloidazaa <eloidaza4@gmail.com>
2026-02-20 14:13:18 +0100

    warning arreglado

--- frontend/src/app/pages/terms-conditions/terms-conditions.component.ts
+++ frontend/src/app/pages/terms-conditions/terms-conditions.component.ts
@@ -1,13 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Necesario para ngIf y ngClass
-import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 import { AppNavbarComponent } from '../../components/app-navbar/app-navbar.component';
 import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
 
 @Component({
   selector: 'app-terms-conditions',
   standalone: true,
-  imports: [CommonModule, RouterLink, AppNavbarComponent, SideMenuComponent],
+  imports: [CommonModule, AppNavbarComponent, SideMenuComponent],
   templateUrl: './terms-conditions.component.html',
   styleUrl: './terms-conditions.component.css'
 })
